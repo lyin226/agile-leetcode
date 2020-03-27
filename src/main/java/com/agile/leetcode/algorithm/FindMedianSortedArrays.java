07f0ca5c2309a08a2cf97e0506434e993b061155
刘义 <liuyi@daling.com>
2020-03-27 12:03:09 +0800

    找出不重复字符的最大子串的长度

---
+++ src/main/java/com/agile/leetcode/algorithm/FindMedianSortedArrays.java
@@ -0,0 +1,25 @@
+package com.agile.leetcode.algorithm;
+
+/**
+ * @author liuyi
+ * @since 2020/3/27
+ * 给定两个大小为 m 和 n 的有序数组 nums1 和 nums2。
+   请你找出这两个有序数组的中位数，并且要求算法的时间复杂度为 O(log(m + n))。
+   你可以假设 nums1 和 nums2 不会同时为空。
+ */
+public class FindMedianSortedArrays {
+
+    public static void main(String[] args) {
+
+    }
+
+//    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+//
+//        for(int i = 0; i < nums1.length; i++) {
+//
+//            for (int j = 0; j < nums2.length; j++) {
+//
+//            }
+//        }
+//    }
+}
---
+++ src/main/java/com/agile/leetcode/algorithm/LengthOfLongestSubstring.java
@@ -0,0 +1,32 @@
+package com.agile.leetcode.algorithm;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author liuyi
+ * @since 2020/3/27
+ *
+ * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+ */
+public class LengthOfLongestSubstring {
+
+    public static void main(String[] args) {
+        String s = "abcab";
+        System.out.println(lengthOfLongestSubstring(s));
+    }
+
+    public static int lengthOfLongestSubstring(String s) {
+        int n = s.length(), ans = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        for (int end = 0, start = 0; end < n; end++) {
+            char alpha = s.charAt(end);
+            if (map.containsKey(alpha)) {
+                start = Math.max(map.get(alpha), start);
+            }
+            ans = Math.max(ans, end - start + 1);
+            map.put(s.charAt(end), end + 1);
+        }
+        return ans;
+    }
+}
